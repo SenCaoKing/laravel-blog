eee04b89cb2e6e66ce1d99a348f72b2ffb04a7e1
SenCaoKing <1134349927@qq.com>
2018-05-29 23:03:23 +0800

    创建文章发布表单验证类

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -51,13 +51,16 @@
             <tr>
                 <th>标签</th>
                 <td>
-                    <input class="form-control" type="text" name="passwords">
+                    @foreach($tag as $v)
+                        {{ $v['name'] }}<input class="form-control" type="checkbox" name="tag_ids[]" value="{{ $v['name'] }}">
+                    @endforeach
+
                 </td>
             </tr>
             <tr>
                 <th>关键词</th>
                 <td>
-                    <input class="form-control" type="text" name="password">
+                    <input class="form-control" type="text" name="keywords">
                 </td>
             </tr>
             <tr>
