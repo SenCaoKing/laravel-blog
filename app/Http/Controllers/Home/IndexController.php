25393a1558744185c0ac93e7edcea687c7fc8617
SenCaoKing <1134349927@qq.com>
2018-05-30 19:49:55 +0800

    Base模型增加常用方法

--- app/Http/Controllers/Home/IndexController.php
+++ app/Http/Controllers/Home/IndexController.php
@@ -9,7 +9,6 @@ class IndexController extends Controller
 {
     public function index()
     {
-        echo phpinfo();
         return view('home/index/index');
     }
 }
--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -9,7 +9,7 @@
 <script src="//cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
 <script src="{{ asset('statics/editormd/lib/marked.min.js') }}"></script>
 <script>
-    var str = '</script>';
+    var str = '<script>';
 var str = marked(str);
 console.log(str);
 $('body').sppend(str);
