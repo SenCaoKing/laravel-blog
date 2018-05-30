c117f389baffc5eacb8fe59fb479db044f391f42
SenCaoKing <1134349927@qq.com>
2018-05-30 20:31:42 +0800

    makedown初步完成

--- resources/views/admin/article/index.blade.php
+++ resources/views/admin/article/index.blade.php
@@ -3,16 +3,29 @@
 <head>
     <meta charset="utf-8">
     <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('statics/prism/prism.min.css') }}">
 </head>
 <body>
 
+<div class="box">
+    {{ htmlspecialchars($article->content) }}
+</div>
+
 <script src="//cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>
+<script src="{{ asset('statics/prism/prism.min.js') }}"></script>
 <script src="{{ asset('statics/editormd/lib/marked.min.js') }}"></script>
 <script>
-    var str = '<script>';
-var str = marked(str);
-console.log(str);
-$('body').sppend(str);
+    {{--var str = '</script>';--}}
+    var str = $('.box').text();
+    console.log(str);
+    marked.setOptions({
+        sanitize: true,
+
+    })
+    var str = marked(str);
+    console.log(str);
+    $('.box').html(str);
+    $('pre').addClass('line-numbers');
 </script>
 </body>
 </html>
