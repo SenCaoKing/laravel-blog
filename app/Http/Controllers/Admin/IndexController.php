f8008cc6b8a3955ee01ecdb815d414901868fd90
SenCaoKing <1134349927@qq.com>
2018-06-03 01:36:22 +0800

    require league/html-to-markdown将ueditor生成的内容转成markdown格式

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -2,13 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Article;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+use League\HTMLToMarkdown\HtmlConverter;
 
 class IndexController extends Controller
 {
+    /**
+     * 后台首页
+     *
+     * @return mixed
+     */
     public function index()
     {
         return view('admin/index/index');
     }
+
 }
