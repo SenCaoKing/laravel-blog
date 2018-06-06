aedfcf160208dfc127e87e37bf121d2a7ed289c1
SenCaoKing <1134349927@qq.com>
2018-06-06 20:33:48 +0800

    文章详情页的上一篇下一篇完成

--- app/Http/Controllers/Home/ArticleController.php
+++ app/Http/Controllers/Home/ArticleController.php
@@ -10,9 +10,15 @@ class ArticleController extends Controller
 {
     public function index($id, Article $article)
     {
+        // 获取文章数据
         $data = $article->getDataById($id);
+        // 获取上一篇
+        $prev = $article->select('id', 'title')->orderBy('created_at', 'asc')->where('id', '>', $id)->limit(1)->first();
+        $next = $article->select('id', 'title')->orderBy('created_at', 'desc')->where('id', '<', $id)->limit(1)->first();
         $assign = [
-            'data' => $data
+            'data' => $data,
+            'prev' => $prev,
+            'next' => $next
         ];
         return view('home/article/index', $assign);
     }
